7e6ad4d4af6b8f91087788c944a26d74549693ea
Niv patel <nivdudhatra3@gmail.com>
2026-08-12 08:15:06 +0530

    Update Celcius.java

--- JAVA/LAB-3/Celcius.java
+++ JAVA/LAB-3/Celcius.java
@@ -1,18 +1,25 @@
 import java.util.*;
 
+class Ferenith
+{
+    public static void findCelsius(double f)
+    {
+        double c = f-((32*5)/9);
+        System.out.println("Celcius is :"+ c);
+    }
+}
+
 public class Celcius
 {
     public static void main(String[] args)
+
     {
         Scanner sc = new Scanner(System.in);
 
-         System.out.println("Enter Ferenith:");
-         
-         int f = sc.nextInt();
+        System.out.println("Enter Ferenith:");
 
-         
-        double c = f-((32*5)/9);
+        double f = sc.nextDouble();
 
-        System.out.println("Celcius is :"+ c);
+        Ferenith.findCelsius(f);
     }
 }
